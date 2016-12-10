b63759c3cdafe4152df4f6e4378bb75eabe91c1a
LorbusChris <c.glombek@t-online.de>
2016-12-10 21:24:31 +0100

    Adds styled-components
    Thanks @lucianlature for leading the way
    (https://github.com/ctrlplusb/react-universally/pull/235)

--- src/shared/components/App/Header/Menu/Menu.js
+++ src/shared/components/App/Header/Menu/Menu.js
@@ -2,12 +2,32 @@
 
 import React from 'react';
 import { Link } from 'found';
+// We use `require` here instead of `import` to fix the issues detailed
+// at https://github.com/styled-components/styled-components/issues/157
+const styled = require('styled-components').default;
 
-function LinkItem(props) {
+const StyledMenu = styled.ul`
+  margin-top: 1rem;
+  background-color: #D4E6F1;
+`;
+
+const StyledLink = styled(Link)`
+  color: #5F6A6A;
+  display: inline-block;
+  margin: 0.5em 0;
+  text-decoration: none;
+  font-family: Helvetica, Arial, sans-serif;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+function StyledLinkItem(props) {
   // TODO: Remove the pragma once evcohen/eslint-plugin-jsx-a11y#81 ships.
   return (
     <li>
-      <Link // eslint-disable-line jsx-a11y/anchor-has-content
+      <StyledLink // eslint-disable-line jsx-a11y/anchor-has-content
         {...props}
         activeStyle={{ fontWeight: 'bold' }}
       />
@@ -17,10 +37,10 @@ function LinkItem(props) {
 
 function Menu() {
   return (
-    <ul style={{ marginTop: '1rem' }}>
-      <LinkItem to="/" exact>Home</LinkItem>
-      <LinkItem to="/about" exact>About</LinkItem>
-    </ul>
+    <StyledMenu>
+      <StyledLinkItem to="/" exact>Home</StyledLinkItem>
+      <StyledLinkItem to="/about" exact>About</StyledLinkItem>
+    </StyledMenu>
   );
 }
 
